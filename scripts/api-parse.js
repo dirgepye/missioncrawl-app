1f99317e17c5f79cad928531ded016acf31eda11
ljmcdade <ljmcdade@gmail.com>
2016-01-06 20:12:05 +0000

    user-signup-login-logout

--- scripts/api-parse.js
+++ scripts/api-parse.js
@@ -1,18 +1,14 @@
-// var Parse = require("parse");
-var Parse = require('parse/node');
-
-
-var your_application_id = "NQATCHki1IGmGfNuoNzgBMhEQrlSNaoMA0kcNExN";
-var your_javascript_key = "ySbNlYIkFzVS2B5R22JtIbYAtJHGGeMfWpfkpzFD";
+var your_application_id = "NC8PhdseSu4eIz7fjSGX6Vxal3qqjfgnVEZjZ0gH";
+var your_javascript_key = "TeJ3i2uljGynZO7wnrb5paUmZnAZjv4YGtzwVrqt";
 
 
 // Initialize Parse with your Parse application javascript keys
 Parse.initialize(your_application_id,
-    your_javascript_key);
+  your_javascript_key);
 
 
 //Models
-var Mission = Parse.Object.extend("Missions");
+var Mission = Parse.Object.extend("Mission");
 var Step = Parse.Object.extend("Step");
 
 
@@ -20,39 +16,115 @@ var Step = Parse.Object.extend("Step");
 //API CALLS
 //////////////////
 
+//Check for Current User
+function checkCurrentUser() {
+  var currentUser = Parse.User.current();
+  if (currentUser) { //user is logged in
+    $("#current-user").html(Parse.User.current().get("username"));
+    console.log("Logged in as " + Parse.User.current().get("username"));
+  }
+  else { // if not logged in, then clear html
+    $("#current-user").html(" ");
+  }
+}
 
-//User signup
+checkCurrentUser();
 
+//User signUp
 
-//User signin
+// var user = new Parse.User();
+// user.set("username", "bsmith");
+// user.set("password", "kitty123");
+// user.set("email", "bob@home.org");
+
+$("#signup").submit(function(event) {
+  event.preventDefault();
+
+  var username = $("#signup-name").val();
+  var password = $("#signup-password").val();
+  var email = $("#signup-email").val();
+
+  var user = new Parse.User();
+  user.set("username", username);
+  user.set("password", password);
+  user.set("email", email);
+
+  user.signUp(null, {
+    success: function(user) {
+      //go to welcome page
+      checkCurrentUser();
+      $("#signup").val(" ");
+    },
+    error: function(user, error) {
+      console.log("signup error" + error.message);
+    }
+  });
+});
+
+// .then(function(user) {
+//   console.log("User signed up!");
+// }, function(error) {
+//   console.log("Error: " + error.code + " " + error.message);
+// });
+
+//User login
+
+$("#login").submit(function(event){
+  event.preventDefault();  //don't refresh browser
+  
+  var username = $("#login-name").val();
+  var password = $("#login-password").val();
+
+Parse.User.logIn(username, password, {
+  success: function(user){
+    checkCurrentUser();
+    console.log("log in successful!");
+  },
+    error: function(user, error){
+      console.log("error");
+    }
+  }
+);
+  // .then(function(user) {
+  //   checkCurrentUser();
+  //   console.log("Logged in successfully!");
+  // });
+});
+
+//User logout
+
+Parse.User.logOut();
+
+var currentUser = Parse.User.current();
 
 
 //List of missions
-var mission_list = new Parse.Query(Mission);
+function getMissionList() {
+  var mission_query = new Parse.Query(Mission);
 
-mission_list.find()
-.then(function(results) {
-  console.log("Successfully retrieved " + results.length + " scores.");
-})
-.then(function(hello) {
-  // Everything is done!
-}, function(error) {
-  console.log("Error: " + error.code + " " + error.message);
-});
-
+  mission_query.find()
+    .then(function(results) {
+      console.log("Successfully retrieved " + results.length + " scores.");
+    })
+    .then(function(hello) {
+      // Everything is done!
+    }, function(error) {
+      console.log("Error: " + error.code + " " + error.message);
+    });
+}
 
 //A mission
 var mission = new Parse.Query(Mission);
 mission.get("mission-id")
-.then(function(mission) {
+  .then(function(mission) {
     // The object was retrieved successfully.
-})
-.then(function(hello) {
-  // Everything is done!
-}, function(object, error) {
+  })
+  .then(function(hello) {
+    // Everything is done!
+  }, function(object, error) {
     // The object was not retrieved successfully.
     // error is a Parse.Error with an error code and message.
-});
+  });
 
 
 
@@ -68,4 +140,4 @@ mission.get("mission-id")
 
 
 
-//Complete a step
+//Complete a step //enter KEY
