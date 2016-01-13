c55b57f0c427685174ee4e396f58ed6362824425
dirgepye <s.stenhouse87@gmail.com>
2016-01-13 17:55:44 +0000

    push

--- js/components/AdventureDetails.js
+++ js/components/AdventureDetails.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import samples from '../adventures';
 import Adventure from './Adventure';
+import AdventuresList from './Adventures'
 
 var AdventureDetails = React.createClass ({
    render() {
        return (
             <div>
-                <div className="crawl">
-                    <div className="crawl__details">
+                <div className="adventure">
+                    <div className="adventure__details">
                     
-                        <div className="crawl-display">
-                            <h2 className="crawl__title">Title here</h2>
-                            <p className="crawl__description">Placeholder description</p>
-                            <div className="crawl-category">
+                        
+                        <div className="adventure-display">
+                            <h2 className="adventure__title">Title here</h2>
+                            <p className="adventure__description">Placeholder dfadsagsd escription</p>
+                            <div className="adventure-category">
                                 <ul>
                                     <li><a href="#">fun, fun, fun!!!</a></li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
+                        
+                        
+                    </div>    
+                    
                     
                     <div className="adventure__steps">
                         <ol>
--- js/components/DisplayAdventures.js
+++ js/components/DisplayAdventures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AdventuresList from '.'
 
 
 // var DisplayButton = React.createClass({
