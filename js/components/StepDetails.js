6c7899645f8eab07a45ef5baac3fcaf9e5a4e058
Ziad Saab <ziad.saab@gmail.com>
2016-01-15 13:52:46 -0500

    test

--- js/components/StepDetails.js
+++ js/components/StepDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import api from '../parse/api';
 
 import {Link,History} from 'react-router';
+import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
 
 var StepDetails = React.createClass({
     mixins : [History],
@@ -37,17 +38,42 @@ var StepDetails = React.createClass({
     render() {
         var title = this.state.step? this.state.step.get('title'): '';
         var description = this.state.step? this.state.step.get('description'):'';
+        var center;
+        if (this.state.step) {
+          var location = this.state.step.get('location');
+          var center = {lat: location.latitude, lng: location.longitude};
+        }
+
         //var title = 'fsdg';
         //var description = 'dsg';
 
         return (
-            <div className="adventure stepdetails">
+          <div>
+            <div style={{float: 'left', marginRight: 20}} className="adventure stepdetails">
                 <ul className="in-progress">
                     <li className="adventure-display in-progress--title"><p>{title}</p></li>
                     <li className="adventure-display in-progress--desc"><p>{description}</p></li>
                 </ul>
                 <button onClick={this.completeStep}>Next Step</button>
             </div>
+            <div style={{overflow: 'hidden'}}>
+              <GoogleMapLoader
+                containerElement={<div style={{height: 400}}/>}
+                googleMapElement={
+                  <GoogleMap
+                    zoom={9}
+                    center={center}
+                    options={{
+                      draggable: false,
+                      mapTypeControl: false,
+                      zoomControl: false
+                    }}
+                  >
+
+                  </GoogleMap>
+                }/>
+            </div>
+          </div>
         )
     }
 })
